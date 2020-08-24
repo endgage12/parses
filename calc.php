7761a463d081f525b6872e7564675b2f3fe08e2c
endgage12 <endgage12@gmail.com>
2020-08-24 10:56:06 +0700

    query database

--- calc.php
+++ calc.php
@@ -1,5 +1,34 @@
 <?php
     include_once "pageTemplate.php";
 
+    $host = "127.0.0.1";
+    $databaseName = "parse";
+    $usr = "root";
+    $password = "";
     
+    //Подключение к базе
+    $connect_db = new mysqli($host, $usr, $password, $databaseName);
+    if($connect_db->connect_errno) {
+        echo "Не удалось подключиться: " . $connect_db->connect_errno;
+        exit;
+    }
+
+    //Занести данные в базу
+    if(!$connect_db->query("INSERT INTO posts (content) VALUES ('texts')")) {
+        echo "Ошибка при выполнении запроса: " . $connect_db->connect_errno;
+        exit;
+    }
+
+    //Обновить (редактировать) данные c условием
+    if(!$connect_db->query("UPDATE posts SET content = 'NewContent' WHERE id > 2")) {
+        echo "Ошибка при выполнении запроса: " . $connect_db->connect_errno;
+        exit;
+    }
+
+    //Вывести неповторяющиеся данные
+    if($result = $connect_db->query("SELECT DISTINCT content FROM posts")) {
+        while($row = $result->fetch_row()) {
+            echo $row[0];
+        }
+    }
 ?>
